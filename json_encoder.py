692bf5fbd9a80bce0c2811d9dfa6321b9efc150f
Matthew Carter <mdotcarter@gmail.com>
2020-12-19 22:23:40 +0000

    Refactor JSONEncoder

--- json_encoder.py
+++ json_encoder.py
@@ -2,10 +2,12 @@ import flask
 
 from delivery_contract_system.contract import Contract
 
+SERIALIZABLE_CUSTOM_CLASSES = [Contract]
+
 
 class JSONEncoder(flask.json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, Contract):
+        if any([isinstance(obj, c) for c in SERIALIZABLE_CUSTOM_CLASSES]):
             return obj.serialize()
 
         return super(MyJSONEncoder, self).default(obj)
